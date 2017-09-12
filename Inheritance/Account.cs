7feb6e0504e93b7416c5a166ec85c97ed2a3ef4a
Greg <gmolsenvideo@gmail.com>
2017-09-12 00:24:09 -0400

    Program demonstrates classes, inheritance, accessibility, and virtual / override methods

---
+++ Inheritance/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance {
+	/// <summary>
+	/// Account class properties are inherited by
+	/// Savings class, but not the other way around.
+	/// </summary>
+	public class Account
+    {
+		public int Number;
+		public string Name;
+		public double Balance;
+
+		public virtual string ToPrint() {
+			return $"{Number}:{Name}-{Balance}";
+		}
+    }
+}
---
+++ Inheritance/Savings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance {
+	/// <summary>
+	/// Savings class inherits properties of account class
+	/// but not the other way around.
+	/// </summary>
+	public class Savings : Account {
+		public double IntRate;
+	
+	public override string ToPrint() {
+		return base.ToPrint() + $"::{IntRate}";
+		}
+	}
+}
